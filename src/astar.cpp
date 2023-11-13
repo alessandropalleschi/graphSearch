adb71fae65fd829e84d4f91ebdf52659eec4d869
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2023-11-13 17:44:28 +0100

    Update astar.cpp

--- src/astar.cpp
+++ src/astar.cpp
@@ -60,3 +60,41 @@ AStar<CostType>::CostType getHeuristic(const Node<CostType>& node, const Node<Co
 
 // Explicit instantiation for int
 template class AStar<int>;
+
+template <typename CostType>
+class Heuristics {
+public:
+    // Define different heuristics as static member functions
+    static CostType simpleHeuristic(const Node<CostType>& current, const Node<CostType>& goal) {
+        
+        return abs(goal.ID-current.ID);
+    }
+    /*
+    static CostType customHeuristic(const Node<CostType>& current, const Node<CostType>& goal) {
+        // Implement another heuristic
+       pass;
+    }
+    */
+    static CostType noHeuristic(const Node<CostType>& current, const Node<CostType>& goal) {
+        // Implement another heuristic
+       return CostType(0);
+    }
+
+    // Add more heuristics as needed
+
+    // Function to get a pointer to a specific heuristic function
+    static std::function<CostType(const Node<CostType>&, const Node<CostType>&)> getHeuristicFunction(int heuristicType) {
+        switch (heuristicType) {
+            case 0:
+                return noHeuristic;
+            case 1:
+                return simpleHeuristic;
+            // Add more cases for other heuristics
+            default:
+                // Default to a simple heuristic if the specified type is not recognized
+                return noHeuristic;
+        }
+    }
+};
+// Explicit instantiation for int
+template class Heuristics<int>;
