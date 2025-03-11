602cf41977fed97d76633a69b6d72a8b6b249c07
Alessandro Palleschi <42964650+alessandropalleschi@users.noreply.github.com>
2025-03-11 10:54:10 +0100

    Update dijkstra.hpp

--- include/dijkstra.hpp
+++ include/dijkstra.hpp
@@ -7,12 +7,14 @@
 
 template <typename CostType>
 class Dijkstra : public SearchAlgorithm<CostType> {
+private:
+    std::unordered_map<Node<CostType>, CostType> costMap; // Added for improved tracking
+
 public:
     using Path = std::vector<Node<CostType>>;
-    Dijkstra(const Graph<CostType>& g);
+    Dijkstra(const Graph<CostType>& graph);
 
     Path search(Node<CostType>& start, Node<CostType>& goal) override;
 };
-
 
 #endif // DIJKSTRA_HPP
